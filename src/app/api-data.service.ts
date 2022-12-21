fca545ad0244b1864d22520f8f839407b2af403f
@manikandan.k1998 <manikandan.k@infrazanalytics.com>
2022-12-21 21:41:45 +0530

    api data configure in service

--- src/app/api-data.service.ts
+++ src/app/api-data.service.ts
@@ -1,15 +1,69 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiDataService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
+  getapidata(): Observable<any> {
+    return this.http.get<any>('./assets/fakeApi.json').pipe(
+      map((result: any) => {
+        result.data = result.data.map((res: any) => {
+          let data = {
+            id: res.id,
+            'user id': res.username,
+            name: res.name,
+            // "website": res.website,
+            'email id': res.email,
+            'phone number': res.phone,
+          };
+          return data;
+        });
 
-  getapidata(): Observable<any>{
-    return this.http.get<any>('./assets/fakeApi.json')
+        
+        /*
+        //column position change
+          type customObj = Record<string, any> | null;
+          const order:Function = ((data: any, order: any) => {
+              return data.map((node:any) => {
+                  return order.reduce((runningValue:any, currentValue:any) => {
+                      return Object.assign(runningValue, { [currentValue]: node?.[currentValue]});
+                  }, {});
+              });
+          })
+
+          const customorder: any = ["username","id","name","website","email","phone"];
+          const ordered: customObj = order(result.data, customorder);
+          return ordered;
+        */
+
+
+        /*
+        //rename key
+          result.data.map((res:any,i:any)=>{
+            const { username, ...rest } = result.data[i];
+          result.data[i] = {
+            "user id": username,
+            ...rest
+          };
+          })
+        */
+
+
+        /*
+        //delete property
+          result.data.map((res:any)=>{
+            delete res.id;
+            return res
+          })
+        */
+
+        return result.data;
+      })
+    );
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,32 +5,41 @@ import { ApiDataService } from './api-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  cols:any=[];
-  products=[]
-  @ViewChild('dt')
-  dt!: Table;
-  constructor(private service:ApiDataService){}
+  cols: any = [];
+  products = [];
+  @ViewChild('dt') dt!: Table;
+  constructor(private service: ApiDataService) {}
 
-  ngOnInit(){
-    this.service.getapidata().subscribe((res:any)=>{
-      this.products=res.data;
-      
-      let arr1:any=[];
-      for(const item of this.products){        
-        arr1=Object.keys(item).map((data)=>data)
+  ngOnInit() {
+    this.service.getapidata().subscribe((res: any) => {
+      this.products = res;
+
+      let arr1: any = [];
+      for (const item of this.products) {
+        arr1 = Object.keys(item).map((data) => data);
       }
-      
-      for(let item of arr1){ 
-        let arr2:any={};
-        arr2.field =  item;
+
+      for (let item of arr1) {
+        let arr2: any = {};
+        arr2.field = item;
         arr2.header = item.toUpperCase();
-        this.cols.push(arr2)
-      }      
-    })
+        this.cols.push(arr2);
+      }
+    });
   }
+
+  // getname(event:any,obj:any){
+  //   if (obj===event.name) {
+  //     if (event.id<=5) {
+  //       return "red"
+  //     }
+  //     return ''
+  //   }
+  //   return''
+  // }
 
   filterGlobal($event: Event, stringVal: any) {
     this.dt.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
